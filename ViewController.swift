2a6856a2176774661017d6a3a5b9ecaacf325566
kimjunseong <kimjunseong@archive-asia.co.jp>
2020-02-05 18:24:33 +0900

    기본 사칙연산 가능 계산기

---
+++ ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by kimjunseong on 2020/02/05.
+//  Copyright © 2020 kimjunseong. All rights reserved.
+//
+
+import UIKit
+
+enum Operation: String {
+    case Add = "+"
+    case Subtract = "-"
+    case Divide = "/"
+    case Multiply = "*"
+    case NULL = "Null"
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var runningNumber = ""
+    var leftValue = ""
+    var rightValue = ""
+    var result = ""
+    var currentOperation: Operation = .NULL
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        outputLbl.text = "0"
+
+    }
+
+    @IBAction func numberPressed(_ sender: RoundButton) {
+        if runningNumber.count <= 8 {
+            runningNumber += "\(sender.tag)"
+            outputLbl.text = runningNumber
+        }
+    }
+    @IBAction func allClearPressed(_ sender: RoundButton) {
+        runningNumber = ""
+        leftValue = ""
+        rightValue = ""
+        result = ""
+        currentOperation = .NULL
+        outputLbl.text = "0"
+    }
+    
+    @IBAction func dotPressed(_ sender: RoundButton) {
+        if runningNumber.count <= 7 {
+            runningNumber += "."
+            outputLbl.text = runningNumber
+        }
+    }
+    
+    @IBAction func equalPressed(_ sender: RoundButton) {
+        operation(operation: currentOperation)
+    }
+    
+    @IBAction func addPressed(_ sender: RoundButton) {
+        operation(operation: .Add)
+    }
+    
+    @IBAction func subtractPressed(_ sender: RoundButton) {
+        operation(operation: .Subtract)
+    }
+    
+    @IBAction func multiplyPressed(_ sender: RoundButton) {
+        operation(operation: .Multiply)
+    }
+    
+    @IBAction func dividePressed(_ sender: RoundButton) {
+        operation(operation: .Divide)
+    }
+    
+    func operation(operation: Operation){
+        if currentOperation != .NULL {
+            if runningNumber != ""{
+                rightValue = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == .Add {
+                    result = "\(Double(leftValue)! + Double(rightValue)!)"
+                } else if currentOperation == .Subtract {
+                     result = "\(Double(leftValue)! - Double(rightValue)!)"
+                } else if currentOperation == .Multiply {
+                     result = "\(Double(leftValue)! * Double(rightValue)!)"
+                } else if currentOperation == .Divide {
+                     result = "\(Double(leftValue)! / Double(rightValue)!)"
+                }
+                leftValue = result
+                if (Double(result)!.truncatingRemainder(dividingBy: 1) == 0){
+                    result = "\(Int(Double(result)!))"
+                }
+                outputLbl.text = result
+            }
+            currentOperation = operation
+            
+        } else {
+            leftValue = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
+}
+
